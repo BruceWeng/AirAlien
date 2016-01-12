a49d460def7e5a8640fd3c194508141580e2636c
BruceWeng <weng8916@gmail.com>
2016-01-11 18:00:38 -0800

    Create Reservations(View)

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   resources :photos
 
   resources :rooms do
-    resources :reservations, only [:create]
+    resources :reservations, only: [:create]
   end
 
 end
