c35ec7fa705dad509983f48065416aef82cbc7f0
BruceWeng <weng8916@gmail.com>
2016-01-12 15:37:59 -0800

    Review(Controller)

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,10 @@ Rails.application.routes.draw do
     resources :messages, only: [:index, :create]
   end
 
+  resources :rooms do
+    resources :reviews, only: [:create, :destroy]
+  end
+
   get '/preload' => 'reservations#preload'
   get '/preview' => 'reservations#preview'
 
