1e92aba699a9556c385bbef53739f3b691a450b5
BruceWeng <weng8916@gmail.com>
2016-01-12 20:32:44 -0800

    Add PayPal with ngrok

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,5 +1,5 @@
 class ReservationsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:notify]
 
   def preload
     room = Room.find(params[:room_id])
@@ -22,11 +22,47 @@ class ReservationsController < ApplicationController
 
   def create
     @reservation = current_user.reservations.create(reservation_params)
-    redirect_to @reservation.room, notice: "Your reservation has been created..."
+
+    if @reservation
+
+      # send request to PayPal
+      values = {
+        business: 'weng8916-facilitator@gmail.com',
+        cmd: '_xclick',
+        upload: 1,
+        notify_url: 'http://########.ngrok.io/notify',
+        amount: @reservation.total,
+        item_name: @reservation.room.listing_name,
+        item_number: @reservation.id,
+        quantity: '1',
+        return: 'http://########.ngrok.io/your_trips'
+      }
+
+      redirect_to "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
+    else
+      redirect_to @reservation.room, alert: "Oops, something went wrong..."
+    end
   end
 
+  protect_from_forgery except: [:notify]
+  def notify
+    params.permit!
+    status = params[:payment_status]
+
+    reservation = Reservation.find(params[:item_number])
+
+    if status = "Completed"
+      reservation.update_attributes status: true
+    else
+      reservation.destroy
+    end
+
+    render nothing: true
+  end
+
+  protect_from_forgery except: [:your_trips]
   def your_trips
-    @trips = current_user.reservations
+    @trips = current_user.reservations.where("status = ?", true)
   end
 
   def your_reservations
--- config/routes.rb
+++ config/routes.rb
@@ -31,4 +31,7 @@ Rails.application.routes.draw do
   get '/your_trips' => 'reservations#your_trips'
   get '/your_reservations' => 'reservations#your_reservations'
 
+  post '/notify' => 'reservations#notify'
+  post '/your_trips' => 'reservations#your_trips'
+
 end
