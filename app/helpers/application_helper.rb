74967418b588173ab57bc0839eb54deccbaea83f
BruceWeng <weng8916@gmail.com>
2016-09-13 15:08:02 -0700

    Fix Gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
     if user.image
       user.image
     else
-      "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical$s=150"
+      "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?s=150"
     end
   end
 end
