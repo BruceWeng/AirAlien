c4f2a1d3278c8f567fed8ad34cd21b6fd7601af8
BruceWeng <weng8916@gmail.com>
2016-09-21 21:58:37 -0700

    reset S3 ENV

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,13 +1,14 @@
 Rails.application.configure do
+  # Sets paperclip to upload images to Amazon S3
   config.paperclip_defaults = {
-    storage: :s3,
-    s3_credentials: {
-      bucket: ENV.fetch('air-alien'),
-      access_key_id: ENV.fetch('AKIAIMLYROVCVMDNVVUQ'),
-      secret_access_key: ENV.fetch('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'),
-      s3_region: ENV.fetch('us-west-1'),
-    }
+  storage: :s3,
+  s3_credentials: {
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
   }
+}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
