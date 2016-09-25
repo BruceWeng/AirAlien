27690290824e2d5b46c188a4fb36a6d6f2e0e46d
BruceWeng <weng8916@gmail.com>
2016-09-24 20:02:49 -0700

    Fix Paypal(Change notify_url to airalien.xyz/notify)

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -30,7 +30,7 @@ class ReservationsController < ApplicationController
         business: 'weng8916-facilitator@gmail.com',
         cmd: '_xclick',
         upload: 1,
-        notify_url: 'http://########.ngrok.io/notify',
+        notify_url: 'http://airalien.xyz/notify',
         amount: @reservation.total,
         item_name: @reservation.room.listing_name,
         item_number: @reservation.id,
